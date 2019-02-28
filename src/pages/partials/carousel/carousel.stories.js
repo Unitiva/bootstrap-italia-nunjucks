7479d14654112792e2a91cc2fad2af55986190a0
Ivan Di Luccio <ivan.diluccio@abstract-technology.com>
2019-02-28 18:02:33 +0100

    Include path fixed

--- src/pages/partials/carousel/carousel.stories.js
+++ src/pages/partials/carousel/carousel.stories.js
@@ -1,7 +1,6 @@
-/* global document */
 import { storiesOf } from '@storybook/html'
 import * as Carousel from './carousel.html'
 
-storiesOf('Partials', module).add('Carousel', () => Carousel, {
+storiesOf('Partials', module).add('Carousel', () => Carousel , {
   notes: 'A very simple component',
 })
--- src/pages/partials/header/header.stories.js
+++ src/pages/partials/header/header.stories.js
@@ -1,7 +1,8 @@
 /* global document */
 import { storiesOf } from '@storybook/html'
 import * as Header from './header.html'
+var nunjucks = require('nunjucks')
 
-storiesOf('Partials', module).add('Header', () => Header, {
+storiesOf('Partials', module).add('Header', () => nunjucks.render('header.html') , {
   notes: 'A very simple component',
 })
