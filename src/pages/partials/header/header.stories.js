42dd08352caf7c034b94315ee1c5dae387913a54
Ivan Di Luccio <ivan.diluccio@abstract-technology.com>
2019-02-28 18:22:39 +0100

    Nunjucks added to storybook

--- src/pages/partials/header/header.stories.js
+++ src/pages/partials/header/header.stories.js
@@ -1,8 +1,7 @@
 /* global document */
 import { storiesOf } from '@storybook/html'
 import * as Header from './header.html'
-var nunjucks = require('nunjucks')
 
-storiesOf('Partials', module).add('Header', () => nunjucks.render('header.html') , {
+storiesOf('Partials', module).add('Header', () => nunjucks.renderString(Header), {
   notes: 'A very simple component',
 })
